246aafcbb36270c1aa70da34b6ce9ae8d8e0b746
liana.valeria15 <liana.valeria15@gmail.com>
2021-11-29 20:52:39 +0000

    initialise server and added routes and controller folder structure

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("hello");
+const express = require("express");
+const routes = require("./routes");
+
+const PORT = 3000;
+
+const app = express();
+
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
