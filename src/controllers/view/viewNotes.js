d16f24becd0e85f7abbcc812f890800b2e48d094
liana.valeria15 <liana.valeria15@gmail.com>
2021-12-04 02:39:52 +0000

    update view note route

--- src/controllers/view/viewNotes.js
+++ src/controllers/view/viewNotes.js
@@ -1,17 +1,17 @@
 //import path
 const path = require("path");
 
-//controller fs to view notes html page
-const viewNotes = (req, res) => {
-  //import notes file
-  const filePath = path.join(__dirname, "../../../public/notes.html");
-  res.sendFile(filePath);
-};
-
 //controller fs to view index page
 const viewIndex = (req, res) => {
   //import index file
   const filePath = path.join(__dirname, "../../../public/index.html");
+  res.sendFile(filePath);
+};
+
+//controller fs to view notes html page
+const viewNotes = (req, res) => {
+  //import notes file
+  const filePath = path.join(__dirname, "../../../public/notes.html");
   res.sendFile(filePath);
 };
 
