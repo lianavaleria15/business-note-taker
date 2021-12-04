6fb33210e6d78da03dce71f677da25f16fcadaf9
liana.valeria15 <liana.valeria15@gmail.com>
2021-12-04 01:16:35 +0000

    created express server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("hello");
+//create an express server
+const express = require("express");
+
+const app = express();
+
+const PORT = 3000;
+app.listen(PORT, () =>
+  console.log(`Server running on htttp://localhost:${PORT}`)
+);
